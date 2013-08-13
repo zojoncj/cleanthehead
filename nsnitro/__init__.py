a8710cf84dc851f1bad4c4d3ad4dae622031204a
Josh Zojonc <Josh.Zojonc@Oregonstate.edu>
2013-08-13 16:26:45 -0600

    Remvoing problem

--- nsnitro/__init__.py
+++ nsnitro/__init__.py
@@ -3,4 +3,3 @@ from nsutil import NSNitroError, NSNitroResponse
 from nsresources import *
 import nsresources
 __all__ = ['NSNitro', 'NSNitroError', 'NSNitroResponse' ] + nsresources.__all__
-__import__("pkg_resources").declare_namespace(__name__)
