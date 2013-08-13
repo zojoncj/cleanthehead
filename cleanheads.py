6f008e1f32e168d41bc5fbe77156b78057ff735f
Josh Zojonc <Josh.Zojonc@Oregonstate.edu>
2013-08-13 17:01:54 -0600

    more work

--- cleanheads.py
+++ cleanheads.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 
-import time
+import os, ConfigParser
 from nsnitro import *
 
-nitro = NSNitro('localhost', 'api_user', 'api_user')
-nitro.login()
+config_file=os.path.dirname(os.path.realpath(__file__))+'/config'
+if(not os.path.isfile(config_file)):
+ print "Config file does not exit!"
+ exit()
+
+config = ConfigParser.ConfigParser()
+config.read(config_file)
+#password = config.get('osu-sig','password')
+#user = config.get('osu-sig','user')
+#host=config.get('osu-sig','host')
+
+lb = NSNitro(config.get('osu-sig','host'),config.get('osu-sig','password'),config.get('osu-sig','user'))
+
+#nitro = NSNitro(host,password,user)
+lb.login()
